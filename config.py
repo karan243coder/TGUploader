cf11adfacd53994d917383e6362ab05a3285222a
karan243coder <152502742+karan243coder@users.noreply.github.com>
2025-01-29 15:44:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ class Config(object):
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", "23171051"))
+    API_HASH = os.environ.get("API_HASH", "10331d5d712364f57ffdd23417f4513c")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5071005351").split())
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
